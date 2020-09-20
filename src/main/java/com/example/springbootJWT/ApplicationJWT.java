5c2784d3d280bbdf9f54b57162e5b295577f3469
aobo25800 <377598262@qq.com>
2020-09-20 15:48:24 +0800

    '初始化'

---
+++ src/main/java/com/example/springbootJWT/ApplicationJWT.java
@@ -0,0 +1,13 @@
+package com.example.springbootJWT;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationJWT {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationJWT.class, args);
+    }
+}
